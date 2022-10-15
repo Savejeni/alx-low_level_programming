7b20a1dabc049e2b4bb87911870981743f614d8c
Saviour <essien.saviour85@gmail.com>
2022-10-15 14:45:54 -0700

    Welcome to C

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+	printf(ALX ACADEMY\n)
+	return (0);
+
+}
