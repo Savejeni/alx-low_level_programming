4ee53f4e696d4f9b782449c52a0ff185b2090108
Saviour <essien.saviour85@gmail.com>
2022-10-17 19:47:29 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Print number in base 16
+ *
+ * Return: Successful alaways
+ */
+int main(void)
+{
+	int num;
+
+	putchar(%xnum);
+	putchar('\n');
+	return (0);
+}
