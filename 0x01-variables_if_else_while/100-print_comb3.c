da0e06e4f12820b3f25cd62c5e979e05fc07bcba
Saviour <essien.saviour85@gmail.com>
2022-10-17 21:24:01 -0700

    100_print_comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Possible combinations of single-digit numbers
+ *
+ * Return: Successful always
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		putchar((num1 % 10) + '0');
+		putchar((num2 % 10) + '0');
+			if (num1 == 8 && num2 == 9)
+				continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
