1381648a92ecd9382874c418c60a764484bef74c
Saviour <essien.saviour85@gmail.com>
2022-10-17 20:09:45 -0700

    Base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int num;
 	char letter;
-	for (num = 0; num<=10; num++)
+	for (num = 0; num <= 10; num++)
 		putchar(num);
-	for (letter = 'a'; letter = 'f'; letter++)
+	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
 	putchar('\n');
 	return (0);
