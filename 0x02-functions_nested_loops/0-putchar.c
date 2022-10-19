94728f86f07345e3655b7a539b260600480a1bcc
Saviour <essien.saviour85@gmail.com>
2022-10-19 08:41:09 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program that prints  \"_putchar\"
+ *
+ * Return: Successful always
+ */
+
+int main(void)
+
+{
+	char letter = _putchar;
+	putchar(letter);
+	putchar('\n');
+	return (0);
+}
