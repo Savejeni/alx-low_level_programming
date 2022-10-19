70227c9f26c96d8622ec85528335df452caeebff
Saviour <essien.saviour85@gmail.com>
2022-10-19 14:57:52 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,21 @@
-#!/bin/bash
-gcc -c main.h
+#include "main.h"
+
+/**
+ * main - print putchar
+ *
+ * Return: Successful always
+ *
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
